89d51e135399f465c2422f383f096d0d8948aedc
And Sandoval <and.sandovala@duocuc.cl>
2025-04-03 00:37:35 -0300

    Cambios andrew
    Cambios

--- Ferremas/server/server.js
+++ Ferremas/server/server.js
@@ -6,22 +6,29 @@ const startServer = (options) => {
 
   const app = express();
 
+  
   app.use(express.static(public_path));
+  app.use('/views', express.static(path.join(__dirname, '../public/views')));
+  app.use('/js', express.static(path.join(__dirname, '../public/js')));
+  app.use('/img', express.static(path.join(__dirname, '../public/img')));
 
-  //Rutas
-  app.get("/login", (req, res) => {
-    const indexPath = path.join(__dirname, "..", public_path, "login.html");
-    res.sendFile(indexPath);
+  // Rutas
+  app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/views/login.html'));
   });
-  app.get("/admin", (req, res) => {
-    const indexPath = path.join(__dirname, "..", public_path, "admin_panel.html");
-    res.sendFile(indexPath);
+
+  app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/views/admin_panel.html'));
   });
+
+  app.get('/vendedor', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/views/vendedor.html'));
+  });
+
+  // Ruta comodín
   app.get("*", (req, res) => {
-    const indexPath = path.join(__dirname, "..", public_path, "login.html");
-    res.sendFile(indexPath);
+    res.sendFile(path.join(__dirname, '../public/views/login.html'));
   });
-
 
   app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
